1deb90c642e9048d8c33aa89110832da55d38c9a
Patrick <patrick881199@gmail.com>
2021-03-09 16:04:17 +1100

    testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,4 @@
 import React from "react";
-import axios from "axios";
 import userEvent from "@testing-library/user-event";
 
 import { render, screen, waitForElementToBeRemoved } from "./custom-render";
@@ -7,10 +6,21 @@ import { render, screen, waitForElementToBeRemoved } from "./custom-render";
 import App from "./App";
 
 describe("<App />", () => {
-  it("Renders <App /> component correctly", async () => {
+  it("Renders <App />, show input form", async () => {
     render(<App />);
     expect(screen.getByText(/Quick Quote/i)).toBeInTheDocument();
   });
 
-  //further functional test needed
+  it("show QuoteDetail infomation when button clicked", async () => {
+    render(<App />);
+
+    userEvent.type(screen.getByLabelText("First Name"), "John");
+    userEvent.type(screen.getByLabelText("Last Name"), "Denver");
+    userEvent.type(screen.getByLabelText("Email"), "JohnDever@gmail.com");
+    userEvent.type(screen.getByLabelText("Telephone / Mobile"), "0439856421");
+    userEvent.type(screen.getByLabelText("Amount"), "100000");
+
+    userEvent.click(screen.getByText(/GET QUOTE/i));
+    await screen.findByText("Term currency is currently not supported");
+  });
 });
